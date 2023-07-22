3d0b266666bcb07c5c4d9398ed33b6679459619a
Vladimir Iakhnev <Yahnev91@yandex.ru>
2023-07-22 20:29:00 +0300

    Задание 1

---
+++ Project1/Project1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int sizeOne;
+	int sizeTwo;
+	int item;
+	std::ifstream file_in("in.txt");
+
+	if (file_in) {
+		
+		file_in >> sizeOne;
+
+		int* arr = new int[sizeOne];
+
+		for (int i = 0; i < sizeOne; i++) {
+			file_in >> item;
+			arr[i] = item;
+		}
+
+		file_in >> sizeTwo;
+
+		int* arrTwo = new int[sizeTwo];
+		for (int i = 0; i < sizeTwo; i++) {
+			file_in >> item;
+			arrTwo[i] = item;
+		}
+
+		std::ofstream file_out("out.txt");
+
+		file_out << sizeTwo << std::endl;
+		file_out << arrTwo[sizeTwo-1] << " ";
+
+		for (int i = 0; i < sizeTwo-1; i++) {
+			file_out << arrTwo[i] << " ";
+		}
+		file_out << std::endl;
+
+		file_out << sizeOne << std::endl;
+
+		for (int i = 1; i < sizeOne; i++) {
+			file_out << arr[i] << " ";
+		}
+		file_out << arr[0] << " ";
+		file_out << std::endl;
+
+		delete[] arr;
+		delete[] arrTwo;
+		file_in.close();
+		file_out.close();
+	}
+	else {
+		std::cout << "Файл не найдет" << std::endl;
+	}
+	return 0;
+}
